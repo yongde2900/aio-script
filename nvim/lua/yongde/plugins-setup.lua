445dd7efc846e740283c945999bb76580e8a7978
yongde <com414141@gmail.com>
2023-04-09 00:18:40 +0800

    add toggleterm and ignore packer_compiled.lua

--- nvim/lua/yongde/plugins-setup.lua
+++ nvim/lua/yongde/plugins-setup.lua
@@ -108,6 +108,12 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	--lazy git
+	use("kdheepak/lazygit.nvim")
+
+	-- toggle term
+	use("akinsho/toggleterm.nvim")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
